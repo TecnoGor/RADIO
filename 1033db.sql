097610d31dbeddb41fb07874a94e2c689775ddd2
TecnoGor <rg740938@gmail.com>
2026-04-21 20:05:32 -0400

    BD

--- 1033db.sql
+++ 1033db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-12-2025 a las 18:21:56
+-- Tiempo de generación: 22-04-2026 a las 02:04:36
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
